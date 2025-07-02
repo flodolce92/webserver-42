38c85740481d8b31191827e6e2f9a7ccc12e4700
Federico Lo Dolce <flodolce92@gmail.com>
2025-07-03 01:04:47 +0200

    feature(Parser): Edited Makefile and main to test Parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
+#include <ConfigParser.hpp>
 #include <Server.hpp>
+#include <iostream>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	if (argc == 2)
+	{
+		try
+		{
+			ConfigParser parser;
+			Config config = parser.parseFile(argv[1]);
+
+			std::cout << "✅ Successfully parsed: " << argv[1] << "\n";
+			parser.printConfig(config);
+			std::cout << std::endl;
+		}
+		catch (const std::exception &e)
+		{
+			std::cout << "❌ Failed to parse " << argv[1] << ":\n";
+			std::cout << "Error: " << e.what() << "\n";
+			return 1;
+		}
+	}
+
 	Server sv = Server(8080);
 	sv.initialize();
 	sv.run();
