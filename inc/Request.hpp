a6d878ce2e25fad399ee60964a9267d8771a31c8
Federico Lo Dolce <flodolce92@gmail.com>
2025-07-03 01:04:47 +0200

    feature(Request Parser): added simple Request class to parse requests

---
+++ inc/Request.hpp
@@ -0,0 +1,45 @@
+#ifndef REQUEST_HPP
+#define REQUEST_HPP
+
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+
+class Request
+{
+private:
+	std::string _rawRequest;
+	std::istringstream _iss;
+	std::string _method;
+	std::string _uri;
+	std::string _version;
+	std::map<std::string, std::string> _headers;
+	std::string _body;
+	bool _isValid;
+	bool _isComplete;
+
+	void parseFirstLine();
+	void parseHeaders();
+	void parseBody();
+
+public:
+	Request(const std::string &rawRequest);
+	~Request();
+
+	// Searches the request headers for the given header name and returns a vector
+	// containing all values associated with that header.
+	// If the header is not found an empty vector is returned.
+	std::vector<std::string> getHeaderValues(const std::string &headerName) const;
+
+	// Getters
+	const std::string &getMethod() const;
+	const std::string &getUri() const;
+	const std::string &getVersion() const;
+	const std::map<std::string, std::string> &getHeaders() const;
+	const std::string &getBody() const;
+	bool isValid() const;
+	bool isComplete() const;
+};
+
+#endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -14,6 +14,7 @@
 #include <signal.h>
 #include <sstream>
 #include <ConfigManager.hpp>
+#include <Request.hpp>
 
 // // Define static const members
 const int Server::_REUSE_ADDR_OPT = 1;
@@ -32,18 +33,18 @@ static std::string toString(T value)
 
 Server::Server(const ConfigManager & configManager): _configManager(configManager)
 {
-	
+
 	// I/O Multiplexing
 	this->_maxFd = -1;
 	FD_ZERO(&this->_readFds);
 	FD_ZERO(&this->_writeFds);
 	FD_ZERO(&this->_exceptFds);
-	
+
 	// Server State
 	this->_running = false;
 	this->_initialized = false;
 	this->_shutdownRequested = false;
-	
+
 	// Timing
 	this->_lastCleanup = time(NULL);
 	this->_timeout.tv_sec = _TIMEOUT_SECONDS;
@@ -109,7 +110,7 @@ bool Server::initialize()
 		memset(&serverAddress, 0, sizeof(serverAddress));
 		serverAddress.sin_family = AF_INET;
 		serverAddress.sin_port = htons(currentConfig.port);
-		
+
 		// Convert the localhost
 		std::string host;
 		if (currentConfig.host == "localhost")
@@ -522,7 +523,8 @@ void Server::processRequest(int clientFd)
 	std::cout << "Client " << clientFd << "'s request: " << std::endl;
 	std::cout << client->getReadBuffer() << std::endl;
 	std::cout << "--------------------------" << std::endl;
-	Response response(client, this->_configManager);
+	// Response response(client, this->_configManager);
+	Request request(client->getReadBuffer());
 
 	client->setState(CONN_WRITING_RESPONSE);
 }
