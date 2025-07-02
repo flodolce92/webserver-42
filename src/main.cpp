746a7362ff1e4d8d7cf930019c004eb0f44b8e93
Federico Lo Dolce <flodolce92@gmail.com>
2025-07-03 01:04:47 +0200

    feature(CGI Handler & Config): Edited main to test new route finding system and added CGI_Handler to Makefile
    - Removed unused methods in CofigManager
    - Tweaked cgi test

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,26 @@ int main(int argc, char *argv[])
 			ConfigManager configManager;
 			configManager.loadConfig(argv[1]);
 
-			std::cout << "✅ Successfully parsed: " << argv[1] << "\\n";
+			std::cout << "✅ Successfully parsed: " << argv[1] << "\n";
 			configManager.printConfiguration();
+			std::cout << std::endl;
+
+			// Test route finder on first server
+			const std::vector<ServerConfig> &servers = configManager.getServers();
+			std::string requestPath = "/api/index.html";
+			if (!servers.empty())
+			{
+				const ServerConfig &server = servers[0];
+				const Route *matchedRoute = server.findMatchingRoute(requestPath);
+				if (matchedRoute)
+				{
+					std::cout << "✅ Matched route for path '" << requestPath << "':\n";
+					std::cout << "Path: " << matchedRoute->path << "\n";
+					std::cout << "Root: " << matchedRoute->root << "\n";
+				}
+				else
+					std::cout << "❌ No matching route found for path '" << requestPath << "' in server: " << server.host << ":" << server.port << "\n";
+			}
 			std::cout << std::endl;
 
 			Server sv = Server(configManager);
@@ -21,14 +39,16 @@ int main(int argc, char *argv[])
 		}
 		catch (const std::exception &e)
 		{
-			std::cout << "❌ Failed to parse " << argv[1] << ":\\n";
-			std::cout << "Error: " << e.what() << "\\n";
+			std::cerr << "❌ Failed to parse " << argv[1] << "\n";
+			std::cerr << e.what() << std::endl;
 			return 1;
 		}
-	} else {
-        std::cerr << "Usage: " << argv[0] << " <config_file_path>" << std::endl;
-        return 1;
-    }
+	}
+	else
+	{
+		std::cerr << "Usage: " << argv[0] << " <config_file_path>" << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
